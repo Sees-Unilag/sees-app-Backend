f9852ae741606ec5f25829ad4c70bc736fce05b2
Kuye Harry <dammykuye@gmail.com>
2023-07-24 18:48:32 +0100

    set up cloudinary module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CoursesModule } from './modules/courses/courses.module';
 import { NotificationsModule } from './modules/notifications/notifications.module';
+import { CloudinaryModule } from './modules/cloudinary/cloudinary.module';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [CoursesModule, NotificationsModule],
+  imports: [CoursesModule, NotificationsModule, CloudinaryModule,
+  ConfigModule.forRoot({isGlobal:true, envFilePath: [".env", ".env.development"]})],
 })
 export class AppModule {}

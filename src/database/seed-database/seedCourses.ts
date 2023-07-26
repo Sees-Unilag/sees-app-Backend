2031f2757ca6f4314477883d9c6429d0d2aae5a3
Kuye Harry <dammykuye@gmail.com>
2023-07-26 13:27:28 +0100

    add seed Courses utility function

--- src/database/seed-database/seedCourses.ts
+++ src/database/seed-database/seedCourses.ts
@@ -1,24 +1,23 @@
-import { PrismaService } from "../prisma.service";
-import { coursesJson } from "./seedCoursesJson";
-
+import { PrismaService } from '../prisma.service';
+import { coursesJson } from './seedCoursesJson';
 
 const prisma = new PrismaService();
 
-const addCourses = async()=>{
-    for(const course of coursesJson){
-        prisma.course.create({
-            data: course
-        })
-    }
-} 
+const addCourses = async () => {
+  for (const course of coursesJson) {
+    prisma.course.create({
+      data: course,
+    });
+  }
+};
 
-const seedCourses = async()=>{
-    try{
-        await addCourses();
-        console.log("Successfully addeD Courses to Database")
-    }catch(err:any){
-        console.log("Error adding Courses to database", err)
-    }
-}
+const seedCourses = async () => {
+  try {
+    await addCourses();
+    console.log('Successfully addeD Courses to Database');
+  } catch (err: any) {
+    console.log('Error adding Courses to database', err);
+  }
+};
 
 seedCourses();

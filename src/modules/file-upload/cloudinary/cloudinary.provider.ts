2e8f95c9d36ebb8246993ae7e2d28231453c701f
Kuye Harry <dammykuye@gmail.com>
2023-07-26 15:28:01 +0100

    add file upload logic

--- src/modules/file-upload/cloudinary/cloudinary.provider.ts
+++ src/modules/file-upload/cloudinary/cloudinary.provider.ts
@@ -1,5 +1,5 @@
 import { v2 as cloudinary } from 'cloudinary';
-import { CLOUDINARY } from '../constants';
+import { CLOUDINARY } from './constants';
 import { ConfigService } from '@nestjs/config/dist';
 
 export const CloudinaryProvider = {
